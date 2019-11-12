865a4979eb4839bb6d165587aeb9b55eee1247b7
Ben Halverson <benhalverson33@gmail.com>
2019-11-11 23:35:13 -0800

    init code for upload demo

---
+++ lib/src/upload/upload.dart
@@ -0,0 +1,35 @@
+import 'dart:html';
+
+import 'package:angular/angular.dart';
+import 'package:angular/core.dart';
+import 'package:dropzone_angular_dart/dropzone_angular_dart.dart';
+
+@Component(
+  selector: 'upload-component',
+  templateUrl: 'upload.html',
+  directives: [DROPZONE_DIRECTIVES, coreDirectives])
+class UploadComponent {
+
+  DropzoneConfiguration dropzoneConfiguration = new DropzoneConfiguration(
+    url: 'http://localhost:8081/upload', addRemoveLinks: true, maxFiles: 1);
+  Dropzone dropzone;
+  Blob uploadedFile = null;
+  String uploadResponse = null;
+  NgZone zone;
+
+  UploadComponent(this.zone);
+
+  void initDropzone(Dropzone dropzone) {
+    this.dropzone = dropzone;
+    this.dropzone.on("success", success);
+  }
+
+  void success(Blob file, String response, Event event) {
+    print('success(' + file.toString() + ", " + response + ", " +
+      event.toString() + ")");
+    zone.run(() {
+      uploadedFile = file;
+      uploadResponse = response;
+    });
+  }
+}
